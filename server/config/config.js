9e88b143edd42794b33289657fa58c84c1232a03
Odalmi <odpaulino2532@gmail.com>
2020-10-04 08:06:45 -0400

    Variable de entorno de MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://comimaster:Odalmi16@cafe.zu2o1.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
